40e51885c53bde5ff3d29157bec358581f32e6aa
sonam <sonamsahu199518@gmail.com>
2022-04-19 21:20:57 +0530

    Add project files.

---
+++ ConstantVariable/Program.cs
@@ -0,0 +1,90 @@
+﻿/*using System;
+public class AreaOfTriangle
+{
+    public static void Main()
+    {
+        const double Base = 2;
+        const double Height = 3;
+
+        Console.WriteLine("the Base is:");
+        Console.WriteLine(Base);
+        Console.WriteLine("the Height is:");
+        Console.WriteLine(Height);
+
+        double Area = (Base * Height) / 2;
+        Console.Write("Area of a Triangle = " + Area);
+        Console.ReadLine();
+    }
+}
+*/
+
+
+/*using System;
+public class VolumOfCube
+{
+    public static void Main()
+    {
+        const int length = 2;
+
+        Console.WriteLine("the length is:");
+        Console.WriteLine(length);
+
+        double volumOfCube = length*length*length;
+        Console.Write(" Volum of a cube = " + volumOfCube);
+        Console.ReadLine();
+    }
+}
+*/
+
+/*using System;
+public class SurfaceAreaOfSphere
+{
+    public static void Main()
+    {
+        const int rad = 2;
+
+        Console.WriteLine("the Radious is:");
+        Console.WriteLine(rad);
+
+        double surfaceAreaOfSphere = 4*3.146*rad*rad;
+        Console.Write(" Surface Area Of Sphere = " + surfaceAreaOfSphere);
+        Console.ReadLine();
+    }
+}*/
+
+/*using System;
+public class areaOfCylinder
+{
+    public static void Main()
+    {
+        const double rad = 2;
+        const double hight = 3;
+
+        Console.WriteLine("the Radious is:");
+        Console.WriteLine(rad);
+        Console.WriteLine("the Hight is:");
+        Console.WriteLine(hight);
+
+        double areaOfCylinder = 2* 3.146 * rad *(rad+hight);
+        Console.Write(" Age Of Cylinder = " + areaOfCylinder);
+        Console.ReadLine();
+    }
+}
+*/
+
+using System;
+public class Perimeter
+{
+    public static void Main()
+    {
+        const int length = 2;
+       
+        Console.WriteLine("the length is:");
+        Console.WriteLine(length);
+
+        double perimeter = 4*length;
+        Console.Write(" Perimeter of surface= " + perimeter);
+        Console.ReadLine();
+    }
+}
+
